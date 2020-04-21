c6a6457e35a516e57505ec9fe1a6da4dc17170f5
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-04-21 09:09:00 +0200

    étape 2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-var app = new Vue({
-  el: '#app',
+new Vue({
+  el: "#app",
   data: {
-    message: 'Hello Vue !',
-    link: 'lien vers le site de grafikart',
-    url_link: 'http://grafikart.fr'
-  }
-})
+    bienvenue: "Hello Vue !",
+    message: "votre message...",
+    link: "lien vers le site de grafikart",
+    url_link: "http://grafikart.fr",
+    success: true,
+    persons: ["Pierre", "Paul", "Jacques"],
+  },
+  methods: {
+    close: function () {
+      this.success = !this.success;
+    },
+    style: function () {
+      if (this.success) {
+        return { background: "#00FF00" };
+      } else {
+        return { background: "#FF0000" };
+      }
+    },
+  },
+});
